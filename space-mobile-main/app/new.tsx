dc7eaa8816e0db63d3e16bc6c85db6337107f8a3
Alexandre Medina <alesurf13@gmail.com>
2023-05-21 12:07:20 -0300

    Create memmory mobile

--- space-mobile-main/app/new.tsx
+++ space-mobile-main/app/new.tsx
@@ -5,17 +5,82 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Image,
 } from 'react-native'
 import NLWLogo from '../src/assets/nlw-logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
 
   const [isPublic, setIsPublic] = useState(false)
+  const [preview, setPreview] = useState(null)
+  const [content, setContent] = useState('')
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const router = useRouter()
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'cover.jpg',
+        type: 'image/jpg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+
+      console.log(coverUrl)
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
@@ -47,19 +112,30 @@ export default function NewMemory() {
         </View>
         <View className="space-y-6">
           <TouchableOpacity
+            onPress={openImagePicker}
             activeOpacity={0.7}
             className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-700 bg-black/20"
           >
-            <View className="flex-row items-center gap-2">
-              <Icon name="image" color="#fff" />
-              <Text className="font-body text-sm text-gray-200">
-                Adicionar foto ou vídeo de capa
-              </Text>
-            </View>
+            {preview ? (
+              <Image
+                source={{ uri: preview }}
+                className="h-full w-full rounded-lg object-cover"
+              />
+            ) : (
+              <View className="flex-row items-center gap-2">
+                <Icon name="image" color="#fff" />
+                <Text className="font-body text-sm text-gray-200">
+                  Adicionar foto ou vídeo de capa
+                </Text>
+              </View>
+            )}
           </TouchableOpacity>
 
           <TextInput
             multiline
+            value={content}
+            onChangeText={setContent}
+            textAlignVertical="top"
             className=" p-0 font-body text-lg text-gray-50"
             placeholderTextColor="#56565a"
             placeholder="Fique livre para adicionar fotos, videos e relatos sobre essa experiència que você quer lembrar para sempre."
@@ -68,6 +144,7 @@ export default function NewMemory() {
           <TouchableOpacity
             activeOpacity={0.7}
             className="items-center self-end rounded-full bg-green-500 px-5 py-2"
+            onPress={handleCreateMemory}
           >
             <Text className="font-alt text-sm uppercase text-black">
               Salvar
