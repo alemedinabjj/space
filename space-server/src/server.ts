0d28e07373dfb45b5526f542d6ed0cdbe7d67737
Alexandre Medina <alesurf13@gmail.com>
2023-05-21 10:05:34 -0300

    uploading stream fastify

--- space-server/src/server.ts
+++ space-server/src/server.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'
 import jwt from '@fastify/jwt'
 
+import multipart from '@fastify/multipart'
+
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
 
 app.register(jwt, {
   secret: 'açsldkfdjnfeinvun22n88',
@@ -16,6 +26,7 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
